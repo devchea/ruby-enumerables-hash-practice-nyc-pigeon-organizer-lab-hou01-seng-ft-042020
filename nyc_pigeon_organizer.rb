24186b2130300a126011f651c8e54cb0fc851904
Richard Chea <rchea817@gmail.com>
2020-04-12 02:33:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,11 +3,17 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
   data.each do |category, attribute|
+    binding.pry
     attribute.each do |attribute_value, array|
+      binding.pry
       array.each do |name|
-        pigeon_list[name] ||= {}
-        pigeon_list[name][category] ||= []
-        pigeon_list[name][category].push(attribute_value.to_s)
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][category] == nil
+          pigeon_list[name][category] = {}
+          binding.pry
+        end
       end
     end
   end
